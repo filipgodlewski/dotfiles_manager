5303566128deb88670d954b5e374c89d58ce150e
Filip Godlewski <54432731+filipgodlewski@users.noreply.github.com>
2021-08-22 22:36:59 +0200

    initial repository structure (#1)
    * feat: setup initial repository structure
    
    * add poetry as the build-backend for my application (instead of standard
    setuptools which was causing some issues for me + I want to learn new
    things)
    * delete entirely setup.py and setup.cfg as with pyproject.toml they are
      totally redundant
    * create basic .gitignore
    * create poetry.lock with initial required packages and their versions
    * move my packages down into src/dotfiles_manager
    * initialize directory 'tests' (empty for now, will be handled with next
      branch focused entirely on unit tests)
    
    * fix: delete Makefile
    
    * I actually don't need it
    
    * fix: remove redundant position in gitignore

---
+++ src/dotfiles_manager/subcommands/submodules.py
@@ -0,0 +1,45 @@
+from os.path import join
+from typing import Tuple
+
+import click
+from dotfiles_manager.utils.dotfiles import Dotfiles
+
+
+@click.command()
+@click.option("--folder", "-f", help="folder to add submodule to", required=True)
+# type=click.Choice([d for d in ctx.DIRS], case_sensitive=True))
+@click.argument("target")
+@click.pass_obj
+def add(ctx: Dotfiles, folder: str, target: str):
+    """Add new submodule under specified directory in your dotfiles."""
+    breakpoint()
+    if folder not in ctx.DIRS:
+        raise KeyError(f"{folder} not in dirs. Use `--help` flag for more information")
+    url, name, relative_path = get_submodule_info(ctx, folder, target)
+    ctx.repo.create_submodule(name=relative_path, path=relative_path, url=url)
+    click.secho("\nAdded new submodule.\n", fg="green")
+    click.secho(f"Name: {name}", fg="yellow")
+    click.echo(f"Path: {relative_path}")
+    click.echo(f"URL: {url}")
+    ctx.repo.submodule_update(recursive=True, force_remove=True, init=True,
+                              force_reset=True)
+    click.secho("\nUpdated recursively all submodules.", fg="green")
+
+
+def get_submodule_info(ctx: Dotfiles, folder: str, target: str) -> Tuple[str, str, str]:
+    """Gets necessary Github information based on the repository name and end dir."""
+
+    cleaned_target: str = target[:-1] if target.endswith(ctx.GH_OBJECT
+                                                         + "/") else target
+    is_gh_domain: bool = cleaned_target.startswith(ctx.GH_DOMAIN)
+    is_gh_object: bool = cleaned_target.endswith(ctx.GH_OBJECT)
+    if is_gh_domain and is_gh_object:
+        url: str = cleaned_target
+    elif is_gh_object:
+        url: str = ctx.GH_DOMAIN + cleaned_target
+    else:
+        url: str = ctx.GH_DOMAIN + cleaned_target + ctx.GH_OBJECT
+    name: str = cleaned_target.split("/")[-1]
+    name: str = name[:-4] if is_gh_object else name
+    relative_path: str = join(folder, ".local", join(ctx.DIRS[folder], name))
+    return url, name, relative_path
